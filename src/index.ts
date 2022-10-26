bb434997b23aa3a4f10670fe9a18378d805dee93
Nghiem Tuan <nghiem.tuan@tda.company>
2022-10-26 17:41:03 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as inquirer from 'inquirer';
+import * as figlet from 'figlet';
+import * as chalk from 'chalk';
 
 const CHOICES = fs.readdirSync(path.join(__dirname, 'templates'));
+const CURR_DIR = process.cwd();
 
+// Define constants
 const QUESTIONS = [
     {
         name: 'template',
@@ -20,6 +24,36 @@ const QUESTIONS = [
     }
 ];
 
-inquirer.prompt(QUESTIONS).then(answers => {
-    console.log(answers)
+// Define interfaces
+interface IAnswer {
+    template: string,
+    name: string,
+}
+
+
+// Define functions
+function createProject(projectPath: string) {
+    if (fs.existsSync(projectPath)) {
+        console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`));
+        return false;
+    }
+    fs.mkdirSync(projectPath);
+
+    return true;
+}
+
+// Handle
+// Show title
+console.log(
+    chalk.yellow(
+        figlet.textSync('Tuntun CLI', { horizontalLayout: 'full' })
+    )
+);
+inquirer.prompt(QUESTIONS).then((answers: IAnswer) => {
+    const {template, name} = answers;
+    const targetPath = path.join(CURR_DIR, name);
+    if (createProject(targetPath)) {
+        return;
+    }
+    console.log(template);
 });
